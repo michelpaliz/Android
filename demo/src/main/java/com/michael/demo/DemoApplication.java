2ca79d85218ff90c546f9f4539e77705ddbc1359
Michael <michelpaliz@hotmail.com>
2022-04-07 00:38:26 +0200

    changes

--- demo/src/main/java/com/michael/demo/DemoApplication.java
+++ demo/src/main/java/com/michael/demo/DemoApplication.java
@@ -1,13 +1,29 @@
 package com.michael.demo;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import com.michael.demo.student.Student;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+	}
+
+	@GetMapping
+	public List<Student> print() {
+		return List.of(new Student(1L, "MARIAM", 21, "maria@gmail.com", LocalDate.of(2000, Month.JANUARY, 5)));
 	}
 
 }
---
+++ demo/src/main/java/com/michael/demo/student/StudentService.java
@@ -0,0 +1,22 @@
+package com.michael.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Service
+public class StudentService {
+    
+    
+    @GetMapping
+    public List<Student> getStudents() {
+
+        return List.of(new Student(1L, "Mariam", 20, "maria@gmail.com", LocalDate.of(2000, Month.APRIL, 5)));
+
+    }
+
+}
